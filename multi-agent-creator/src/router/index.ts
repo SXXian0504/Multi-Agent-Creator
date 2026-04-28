81ed2ff73c34698b2160e0f61d8fde1c2d627a29
SXXian0504 <13610297143@163.com>
2026-04-29 00:43:16 +0800

    feat:代码重构-新增实体类、mapper接口，新建Vue项目，初始化前端界面

---
+++ multi-agent-creator/src/router/index.ts
@@ -0,0 +1,27 @@
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: '主页',
+            component: HomePage,
+        },
+        {
+            path: '/user/login',
+            name: '用户登录',
+            component: UserLoginPage,
+        },
+        {
+            path: '/user/register',
+            name: '用户注册',
+            component: UserRegisterPage,
+        },
+        {
+            path: '/admin/userManage',
+            name: '用户管理',
+            component: () => import('@/pages/admin/UserManagePage.vue'),
+        },
+    ],
+})
+
+export default router
